1e9c40f716ceed56eb2c093fba410698f501fde8
bauerpm <bauerpm@gmail.com>
2018-07-28 11:10:02 -0400

    added navigation to app.js added actions and action types worked on NewQuestions and Quiz components.

--- App.js
+++ App.js
@@ -1,19 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View} from 'react-native';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from '../reducers/index.js';
+import {StackNavigator, TabNavigator} from 'react-navigation';
+import {setNotification} from '../api/notification';
+import NewDeck from './deck/NewDeck';
+import DeckList from './deck/DeckList.js';
+import IndividualDeck from './deck/IndividualDeck.js';
+import NewQuestion from './NewQuestion';
+import Quiz from './Quiz.js'
+
+const Tabs = TabNavigator({
+        DeckList: {
+            screen: DeckList,
+            navigationOptions: {
+                tabBarLabel: 'All Decks'
+            },
+        },
+        NewDeck: {
+            screen: NewDeck,
+            navigationOptions: {
+                tabBarLabel: 'New Deck',
+            },
+        },
+    }
+);
+
+const AppNavigator = StackNavigator({
+    Home: {
+        screen: Tabs,
+        navigationOptions: {title: 'Home'},
+    },
+    IndividualDeck: {
+        screen: IndividualDeck,
+        navigationOptions: {
+            headerTintColor: '#000',
+        },
+    },
+    Quiz: {
+        screen: Quiz,
+        navigationOptions: {
+            title: 'Quiz',
+            headerTintColor: '#000',
+        },
+    },
+    NewQuestion: {
+        screen: NewQuestion,
+        navigationOptions: {
+            title: 'Add Question',
+            headerTintColor: '#000',
+        },
+    },
+});
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        
-      </View>
-    );
-  }
-}
+    componentDidMount() {
+        setNotification();
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+    render() {
+        return <Provider store={createStore(reducer)}>
+            <View style={{flex: 1}}>
+                <AppNavigator />
+            </View>
+        </Provider>
+    }
+}
